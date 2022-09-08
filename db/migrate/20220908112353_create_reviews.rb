34dddb9b23601e54bcd8b0d8aebda7c599ad7a7e
Anthony Kimeu <tkymeu@gmail.com>
2022-09-08 16:57:31 +0300

    updated backend

--- db/migrate/20220908112353_create_reviews.rb
+++ db/migrate/20220908112353_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.string :text
+      t.string :body
       t.integer :book_id
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# Book.create(title: "Breath of the Wild", author: "Ian", likes: 60)
+# Book.create(title: "Delta Force", author: "Mark", likes: 45)
+# Book.create(title: "Breath of the Wild", author: "Cate", likes: 54)
+# Book.create(title: "Breath of the Wild", author: "Brandon", likes: 30)
 
 puts "✅ Done seeding!"
